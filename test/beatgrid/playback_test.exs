1610f3dae7f04823a91b44b55cb0dd7bb6ed7ffb
Lucas Tavano <lucas.c.tavano@gmail.com>
2026-06-26 10:42:50 -0300

    test(playback): group tests by function per project convention

--- test/beatgrid/playback_test.exs
+++ test/beatgrid/playback_test.exs
@@ -3,11 +3,15 @@ defmodule Beatgrid.PlaybackTest do
 
   alias Beatgrid.Playback
 
-  test "preview_offset_ms is the start offset for a preview play" do
-    assert Playback.preview_offset_ms() == 20_000
+  describe "preview_offset_ms/0" do
+    test "returns the configured start offset in milliseconds" do
+      assert Playback.preview_offset_ms() == 20_000
+    end
   end
 
-  test "preview_min_duration_ms is the minimum track length for the preview jump" do
-    assert Playback.preview_min_duration_ms() == 25_000
+  describe "preview_min_duration_ms/0" do
+    test "returns the minimum track length for the preview jump" do
+      assert Playback.preview_min_duration_ms() == 25_000
+    end
   end
 end
